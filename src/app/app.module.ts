2637c3101dada11f8ebbeb1a81c2a92373eb485b
Abilash20213558 <abiabi1106@gmail.com>
2022-01-13 11:47:23 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BoxComponent } from './box/box.component';
+import { SmiddleComponent } from './smiddle/smiddle.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [ 
+  {path:'foot',component: FooterComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BoxComponent,
+    SmiddleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
